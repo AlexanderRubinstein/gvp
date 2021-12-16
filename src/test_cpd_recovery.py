d66830c0f7e39f2c3d78a55e32d1328256b4efa3
Alexander Rubinstein <rubalex14@gmail.com>
2021-12-16 21:55:59 +0300

    update test_cpd_recovery.py

--- src/test_cpd_recovery.py
+++ src/test_cpd_recovery.py
@@ -2,12 +2,15 @@ import sys
 
 from datetime import datetime
 from datasets import *
-import random, pdb, sys
+import random, pdb
 import numpy as np
 import tqdm, util
 from models import *
 import argparse
+import os
 
+
+FEATURIZER_PATH = os.path.join("..", "models", "cath_pretrained")
 
 def recovery(designs, orig):
     N, L = designs.shape
@@ -22,25 +25,41 @@ def sample(sampling_method, structure, mask, n, T=0.1): # [1, N, 4, 3]
 
 def make_parser():
     parser = argparse.ArgumentParser(description='test cpd script parser')
-    parser.add_argument('--model_path', type=str,
-                    help='path to the model to eval')
+    parser.add_argument('--model_path', type=str, default=None,
+                    help='path to the model to test')
     parser.add_argument('--test_type', type=str,
                     help='what kind of test set to use', choices=["cath", "short", "sc", "ts50"])
     parser.add_argument('--output_file', type=str,
                     help='output file')
     parser.add_argument('--max_structs', type=int, default=0,
                     help='max number of structs to test on')
+    parser.add_argument('--pairwise', action="store_true",
+                    help='whether to use pairwise model')
+    parser.add_argument('--n_runs', type=int, default=100,
+                    help='number of runs to calculate mean result from')
     return parser
 
 if __name__ == "__main__":
     args = make_parser().parse_args()
 
-    model = CPDModel(node_features=(8, 100), edge_features=(1,32), hidden_dim=(16,100))
+    if not args.pairwise and args.model_path is None:
+        raise Exception("No model filepath for original model")
+
     optimizer = tf.keras.optimizers.Adam()
 
-    util.load_checkpoint(model, optimizer, args.model_path)
+    featurizer = CPDModel(node_features=(8, 100), edge_features=(1,32), hidden_dim=(16,100))
 
-    pairwise_model = PairwiseCPDModel(model, num_letters=20, hidden_dim=(16,100), copy_top_gvp=True)
+    if args.pairwise:
+
+        util.load_checkpoint(featurizer, optimizer, FEATURIZER_PATH)
+
+        model = PairwiseCPDModel(featurizer, num_letters=20, hidden_dim=(16,100), copy_top_gvp=True)
+
+        if args.model_path:
+            util.load_checkpoint(model, optimizer, args.model_path)
+    else:
+        model = featurizer
+        util.load_checkpoint(model, optimizer, args.model_path)
 
     if args.test_type == "cath":
         _, _, testset = cath_dataset(1)
@@ -53,22 +72,18 @@ if __name__ == "__main__":
 
     num = 0
     for structure, seq, mask in tqdm.tqdm(testset):
-        print("structure.shape:", structure.shape)
-        print("seq.shape:", seq.shape)
-        print("mask.shape:", mask.shape)
 
         num += 1
         length = seq.shape[1]
-        # N = 100
-        N = 1
+
+        N = args.n_runs
 
         n = min(int(20000 / length), N)
         design = np.zeros((N, length))
         idx = 0
         while (idx < N):
             my_n = min(n, N-idx)
-            # pred = sample(model.sample, structure, mask, my_n)
-            pred = sample(pairwise_model.sample_independently, structure, mask, my_n)
+            pred = sample(model.sample_independently if args.pairwise else model.sample, structure, mask, my_n)
             design[idx:idx+my_n] = tf.cast(pred, tf.int32).numpy()
             idx += min(n, N-idx)
 
